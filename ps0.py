0100ea17ea6892e2f917d61a52554ec87d754d5f
Jason_Engineer <jacksonly@users.noreply.github.com>
2017-08-19 03:44:49 -0500

    Add files via upload
    第一次课程作业

---
+++ ps0.py
@@ -0,0 +1,9 @@
+ # Problem Set 0
+ # Name: Yan Lu
+ # Collaborators: No one
+ # Time: 12:55 - 13:03
+ #
+
+lastName = input('Enter your last name: ')
+firstName = input('Enter your first name: ')
+print(firstName + lastName)
